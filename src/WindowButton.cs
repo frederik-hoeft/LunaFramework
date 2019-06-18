be9d86f3426f432a2ecc3652a669e79e862c92a8
Th3-Fr3d <43813142+Th3-Fr3d@users.noreply.github.com>
2019-06-18 19:50:14 +0200

    Added Animation For WindowButton

--- src/WindowButton.cs
+++ src/WindowButton.cs
@@ -16,10 +16,34 @@ namespace CustomMetroForms
         private Image HoverImage;
         private Color NormalColor;
         private Color HoverColor;
-        public event EventHandler<EventArgs> OnClickEvent;
+        private Color CurrentColor;
+        private int steps = 20;
+        private int step = 0;
+        private int _AnimationInterval = 10;
+        public event EventHandler OnClickEvent;
+        private Timer AnimationTimer = new Timer();
+        private bool timerRunning = false;
+        private bool hasFocus = false;
+        private int astep, rstep, gstep, bstep;
         public WindowButton()
         {
             InitializeComponent();
+            AnimationTimer.Tick += new EventHandler(AnimationTimer_Tick);
+            AnimationTimer.Interval = _AnimationInterval;
+        }
+
+        [DefaultValue(10)]
+        public int AnimationInterval
+        {
+            get { return _AnimationInterval; }
+            set { _AnimationInterval = value; AnimationTimer.Interval = value; }
+        }
+
+        [DefaultValue(20)]
+        public int Steps
+        {
+            get { return steps; }
+            set { steps = value; }
         }
 
         public Image ImageNormal
@@ -46,16 +70,160 @@ namespace CustomMetroForms
             set { HoverColor = value; }
         }
 
+        private void AnimationTimer_Tick(object sender, EventArgs e)
+        {
+            int A, R, G, B;
+            if (hasFocus)
+            {
+                if (HoverColor.A > CurrentColor.A)
+                {
+                    A = (CurrentColor.A + astep > HoverColor.A ? HoverColor.A : CurrentColor.A + astep);
+                }
+                else if (HoverColor.A < CurrentColor.A)
+                {
+                    A = (CurrentColor.A + astep < HoverColor.A ? HoverColor.A : CurrentColor.A + astep);
+                }
+                else
+                {
+                    A = HoverColor.A;
+                }
+                if (HoverColor.R > CurrentColor.R)
+                {
+                    R = (CurrentColor.R + rstep > HoverColor.R ? HoverColor.R : CurrentColor.R + rstep);
+                }
+                else if (HoverColor.R < CurrentColor.R)
+                {
+                    R = (CurrentColor.R + rstep < HoverColor.R ? HoverColor.R : CurrentColor.R + rstep);
+                }
+                else
+                {
+                    R = HoverColor.R;
+                }
+                if (HoverColor.G > CurrentColor.G)
+                {
+                    G = (CurrentColor.G + gstep > HoverColor.G ? HoverColor.G : CurrentColor.G + gstep);
+                }
+                else if (HoverColor.G < CurrentColor.G)
+                {
+                    G = (CurrentColor.G + gstep < HoverColor.G ? HoverColor.G : CurrentColor.G + gstep);
+                }
+                else
+                {
+                    G = HoverColor.G;
+                }
+                if (HoverColor.B > CurrentColor.B)
+                {
+                    B = (CurrentColor.B + bstep > HoverColor.B ? HoverColor.B : CurrentColor.B + bstep);
+                }
+                else if (HoverColor.B < CurrentColor.B)
+                {
+                    B = (CurrentColor.B + bstep < HoverColor.B ? HoverColor.B : CurrentColor.B + bstep);
+                }
+                else
+                {
+                    B = HoverColor.B;
+                }
+            }
+            else
+            {
+                if (NormalColor.A > CurrentColor.A)
+                {
+                    A = (CurrentColor.A + astep > NormalColor.A ? NormalColor.A : CurrentColor.A + astep);
+                }
+                else if (NormalColor.A < CurrentColor.A)
+                {
+                    A = (CurrentColor.A + astep < NormalColor.A ? NormalColor.A : CurrentColor.A + astep);
+                }
+                else
+                {
+                    A = NormalColor.A;
+                }
+                if (NormalColor.R > CurrentColor.R)
+                {
+                    R = CurrentColor.R + rstep > NormalColor.R ? NormalColor.R : CurrentColor.R + rstep;
+                }
+                else if (NormalColor.R < CurrentColor.R)
+                {
+                    R = CurrentColor.R + rstep < NormalColor.R ? NormalColor.R : CurrentColor.R + rstep;
+                }
+                else
+                {
+                    R = NormalColor.R;
+                }
+                if (NormalColor.G > CurrentColor.G)
+                {
+                    G = CurrentColor.G + gstep > NormalColor.G ? NormalColor.G : CurrentColor.G + gstep;
+                }
+                else if (NormalColor.G < CurrentColor.G)
+                {
+                    G = CurrentColor.G + gstep < NormalColor.G ? NormalColor.G : CurrentColor.G + gstep;
+                }
+                else
+                {
+                    G = NormalColor.G;
+                }
+                if (NormalColor.B > CurrentColor.B)
+                {
+                    B = CurrentColor.B + bstep > NormalColor.B ? NormalColor.B : CurrentColor.B + bstep;
+                }
+                else if (NormalColor.B < CurrentColor.B)
+                {
+                    B = CurrentColor.B + bstep < NormalColor.B ? NormalColor.B : CurrentColor.B + bstep;
+                }
+                else
+                {
+                    B = NormalColor.B;
+                }
+            }
+            CurrentColor = Color.FromArgb(R, G, B);
+            BackColor = CurrentColor;
+            if (hasFocus)
+            {
+                if (CurrentColor.Equals(HoverColor))
+                {
+                    AnimationTimer.Stop();
+                    timerRunning = false;
+                }
+            }
+            else
+            {
+                if (CurrentColor.Equals(NormalColor))
+                {
+                    AnimationTimer.Stop();
+                    timerRunning = false;
+                }
+            }
+            this.Invalidate();
+        }
+
         private void MouseEnterEvent()
         {
-            pictureBox.Image = HoverImage;
-            this.BackColor = HoverColor;
+            CurrentColor = BackColor;
+            hasFocus = true;
+            astep = Convert.ToInt32(HoverColor.A - NormalColor.A > 0 ? Math.Ceiling((double)(HoverColor.A - NormalColor.A) / (double)steps) : Math.Floor((double)(HoverColor.A - NormalColor.A) / (double)steps));
+            rstep = Convert.ToInt32(HoverColor.R - NormalColor.R > 0 ? Math.Ceiling((double)(HoverColor.R - NormalColor.R) / (double)steps) : Math.Floor((double)(HoverColor.R - NormalColor.R) / (double)steps));
+            gstep = Convert.ToInt32(HoverColor.G - NormalColor.G > 0 ? Math.Ceiling((double)(HoverColor.G - NormalColor.G) / (double)steps) : Math.Floor((double)(HoverColor.G - NormalColor.G) / (double)steps));
+            bstep = Convert.ToInt32(HoverColor.B - NormalColor.B > 0 ? Math.Ceiling((double)(HoverColor.B - NormalColor.B) / (double)steps) : Math.Floor((double)(HoverColor.B - NormalColor.B) / (double)steps));
+            if (!timerRunning)
+            {
+                timerRunning = true;
+                AnimationTimer.Start();
+            }
         }
 
         private void MouseLeaveEvent()
         {
-            pictureBox.Image = NormalImage;
-            this.BackColor = NormalColor;
+            CurrentColor = BackColor;
+            hasFocus = false;
+            astep = Convert.ToInt32(NormalColor.A - HoverColor.A > 0 ? Math.Ceiling((double)(NormalColor.A - HoverColor.A) / (double)steps) : Math.Floor((double)(NormalColor.A - HoverColor.A) / (double)steps));
+            rstep = Convert.ToInt32(NormalColor.R - HoverColor.R > 0 ? Math.Ceiling((double)(NormalColor.R - HoverColor.R) / (double)steps) : Math.Floor((double)(NormalColor.R - HoverColor.R) / (double)steps));
+            gstep = Convert.ToInt32(NormalColor.G - HoverColor.G > 0 ? Math.Ceiling((double)(NormalColor.G - HoverColor.G) / (double)steps) : Math.Floor((double)(NormalColor.G - HoverColor.G) / (double)steps));
+            bstep = Convert.ToInt32(NormalColor.B - HoverColor.B > 0 ? Math.Ceiling((double)(NormalColor.B - HoverColor.B) / (double)steps) : Math.Floor((double)(NormalColor.B - HoverColor.B) / (double)steps));
+            if (!timerRunning)
+            {
+                timerRunning = true;
+                AnimationTimer.Start();
+            }
         }
 
         private void WindowButton_MouseEnter(object sender, EventArgs e)
@@ -70,7 +238,7 @@ namespace CustomMetroForms
 
         private void WindowButton_Click(object sender, EventArgs e)
         {
-            OnClickEvent(this, e);
+            ClickEvent(e);
         }
 
         private void tableLayoutPanel_MouseEnter(object sender, EventArgs e)
@@ -85,7 +253,12 @@ namespace CustomMetroForms
 
         private void tableLayoutPanel_Click(object sender, EventArgs e)
         {
-            OnClickEvent(this, e);
+            ClickEvent(e);
+        }
+
+        protected virtual void ClickEvent(EventArgs e)
+        {
+            OnClickEvent?.Invoke(this, e);
         }
 
         private void pictureBox_MouseEnter(object sender, EventArgs e)
@@ -100,7 +273,7 @@ namespace CustomMetroForms
 
         private void pictureBox_Click(object sender, EventArgs e)
         {
-            OnClickEvent(this, e);
+            ClickEvent(e);
         }
     }
 }
